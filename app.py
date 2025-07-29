19e4dcb24b47f73f1f42ff638727240962fd2ef3
AdarshChetri <chetriadarsh3@gmail.com>
2025-07-29 16:26:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,10 +22,7 @@ def home():
         except Exception as e:
             caption = f"Error: {e}"
     return render_template("index.html", caption=caption)
-import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
-app.run
-
